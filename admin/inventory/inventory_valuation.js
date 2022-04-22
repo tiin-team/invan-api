73fc7ffe7057ae1ea11719cc80fad505e1403c44
umarkhan <creativeboy1999@gmail.com>
2022-04-22 14:50:12 +0500

    inv valuation

--- admin/inventory/inventory_valuation.js
+++ admin/inventory/inventory_valuation.js
@@ -244,6 +244,251 @@ async function inventoryValuationResult({ limit, page, supplier_id, organization
     data: items,
   }
 }
+async function inventoryValuationResultByPrimarySupplier({ limit, page, supplier_id, organization, service }, instance,) {
+
+  const query = { $match: { organization } };
+
+  if (supplier_id) {
+    try {
+      query["$match"].primary_supplier_id = instance.ObjectId(supplier_id);
+    } catch (error) { }
+  }
+  const unwindServices = { $unwind: { path: "$services" } };
+
+  const projectPrimaryFields = {
+    $project: {
+      name: "$name",
+      has_variants: "$has_variants",
+      item_type: "$item_type",
+      barcode: "$barcode",
+      sku: "$sku",
+      cost: "$cost",
+      primary_supplier_id: "$primary_supplier_id",
+      variant_items: "$variant_items",
+      service: "$services.service",
+      in_stock: {
+        $max: [0, "$services.in_stock"],
+        // $max: [0, { $round: ["$services.in_stock", 3] }],
+      },
+      inventory: {
+        $multiply: [
+          { $max: ["$cost", 0] },
+          { $max: ["$services.in_stock", 0] },
+        ],
+      },
+      retail: {
+        $multiply: [
+          { $max: ["$services.price", 0] },
+          { $max: ["$services.in_stock", 0] },
+        ],
+      },
+      potential: {
+        $subtract: [
+          {
+            $multiply: [
+              { $max: ["$services.price", 0] },
+              { $max: ["$services.in_stock", 0] },
+            ],
+          },
+          {
+            $multiply: [
+              { $max: ["$cost", 0] },
+              { $max: ["$services.in_stock", 0] },
+            ],
+          },
+        ],
+      },
+    },
+  };
+
+  const joinItems = {
+    $group: {
+      _id: "$primary_supplier_id",
+      name: {
+        $first: "$name",
+      },
+      has_variants: {
+        $first: "$has_variants",
+      },
+      item_type: {
+        $first: "$item_type",
+      },
+      barcode: {
+        $first: "$barcode",
+      },
+      sku: {
+        $first: "$sku",
+      },
+      variant_items: {
+        $first: "$variant_items",
+      },
+      cost: {
+        $first: "$cost",
+      },
+      inventory: {
+        $sum: "$inventory",
+      },
+      in_stock: {
+        $sum: "$in_stock",
+      },
+      retail: {
+        $sum: "$retail",
+      },
+      potential: {
+        $sum: "$potential",
+      },
+    },
+  };
+
+  service = instance.ObjectId(service);
+
+  let is_service = {
+    $and: [
+      { $ne: ["$service", service] },
+      { $ne: ["$service", service + ""] },
+    ],
+  };
+  service = service + "";
+  if (typeof service == typeof "invan" && service != "") {
+    try {
+      service = instance.ObjectId(service);
+    } catch (error) { }
+    joinItems["$group"] = {
+      _id: "$primary_supplier_id",
+      name: {
+        $first: "$name",
+      },
+      cost: {
+        $first: "$cost",
+      },
+      has_variants: {
+        $first: "$has_variants",
+      },
+      item_type: {
+        $first: "$item_type",
+      },
+      barcode: {
+        $first: "$barcode",
+      },
+      sku: {
+        $first: "$sku",
+      },
+      variant_items: {
+        $first: "$variant_items",
+      },
+      in_stock: {
+        $sum: {
+          $cond: [is_service, 0, "$in_stock"],
+        },
+      },
+      inventory: {
+        $sum: {
+          $cond: [is_service, 0, "$inventory"],
+        },
+      },
+      retail: {
+        $sum: {
+          $cond: [is_service, 0, "$retail"],
+        },
+      },
+      potential: {
+        $sum: {
+          $cond: [is_service, 0, "$potential"],
+        },
+      },
+    };
+  }
+  const items = await instance.goodsSales
+    .aggregate([
+      query,
+      { $skip: limit * (page - 1) },
+      { $limit: limit },
+      unwindServices,
+      projectPrimaryFields,
+      joinItems,
+      { $sort: { _id: 1 } },
+    ])
+    .allowDiskUse(true)
+    .exec();
+
+  const calculateTotal = {
+    $group: {
+      _id: null,
+      inventory: {
+        $sum: {
+          $cond: ["$has_variants", 0, "$inventory"],
+        },
+      },
+      retail: {
+        $sum: {
+          $cond: ["$has_variants", 0, "$retail"],
+        },
+      },
+      potential: {
+        $sum: {
+          $cond: ["$has_variants", 0, "$potential"],
+        },
+      },
+    },
+  };
+
+  service = service + "";
+  if (typeof service == typeof "invan" && service != "") {
+    try {
+      service = instance.ObjectId(service);
+    } catch (error) { }
+    calculateTotal["$group"] = {
+      _id: null,
+      inventory: {
+        $sum: {
+          $cond: [{ $or: [is_service, "$has_variants"] }, 0, "$inventory"],
+        },
+      },
+      retail: {
+        $sum: {
+          $cond: [{ $or: [is_service, "$has_variants"] }, 0, "$retail"],
+        },
+      },
+      potential: {
+        $sum: {
+          $cond: [{ $or: [is_service, "$has_variants"] }, 0, "$potential"],
+        },
+      },
+    };
+  }
+
+  let total = await instance.goodsSales
+    .aggregate([
+      query,
+      unwindServices,
+      projectPrimaryFields,
+      calculateTotal,
+    ])
+    .allowDiskUse(true)
+    .exec();
+  if (!(total instanceof Array) || total.length == 0) {
+    total = [{
+      inventory: 0,
+      margin: 0,
+      page: 1,
+      potential: 0,
+      retail: 0,
+    }];
+  }
+  const total_items = await instance.goodsSales
+    .countDocuments(query["$match"])
+    .exec();
+
+  return {
+    ...total[0],
+    total: total_items,
+    margin:
+      (total[0].inventory != 0 ? total[0].potential / total[0].inventory : 0) *
+      100,
+    page: Math.ceil(total_items / limit),
+    data: items,
+  }
+}
 async function inventoryValuationResultBySupplier({ limit, page, supplier_id, organization, service }, instance,) {
 
   const query = {
@@ -993,7 +1238,7 @@ module.exports = fp((instance, options, next) => {
 
           const user = request.user;
 
-          const result = await inventoryValuationResultBySupplier({
+          const result = await inventoryValuationResultByPrimarySupplier({
             limit, page,
             supplier_id,
             organization: user.organization,
@@ -1024,55 +1269,7 @@ module.exports = fp((instance, options, next) => {
       return reply;
     }
   );
-  instance.get(
-    "/inventory/valuation",
-    {
-      version: '2.0.0',
-      schema: {
-        query: {
-          type: "object",
-          properties: {
-            limit: { type: "integer", minimum: 1 },
-            page: { type: "integer", minimum: 1 },
-            service: { type: "string" },
-            supplier_id: { type: "string" },
-          },
-          required: [],
-          additionalProperties: false,
-        },
-      },
-    },
-    async (request, reply) => {
-      instance.authorization(request, reply, async () => {
-        try {
-          const { supplier_id, service } = request.query;
-          const limit = isNaN(parseInt(request.query.limit))
-            ? 10
-            : parseInt(request.query.limit)
-          const page = isNaN(parseInt(request.query.page))
-            ? 1
-            : parseInt(request.query.page)
-
-          const user = request.user;
-
-          const result = await inventoryValuationResultPartiation({
-            limit, page,
-            supplier_id,
-            organization: user.organization,
-            service
-          }, instance, 'supplier')
-
-          reply.ok(result);
-        } catch (error) {
-          reply.error(error.message)
-        }
-        return reply;
-      })
-
-      return reply;
-    }
-  );
-
+  // xato qilingan
   instance.get(
     "/inventory/valuation/:supplier_id",
     {
