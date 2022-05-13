52a660b07922a412bd99a64bfa4a0ff94c8b0d74
umarkhan <creativeboy1999@gmail.com>
2022-05-13 15:48:35 +0500

    -a get purchase pdf

--- supplier/routes/index.js
+++ supplier/routes/index.js
@@ -4,6 +4,7 @@ const sign = require('./sign');
 const transactions = require('./transactions');
 const valuation = require('./valuation');
 const dashboard = require('./dashboard');
+const purchase = require('./purchase');
 const report_sales_by_item = require('./reports/sales.by_item');
 
 const fp = require('fastify-plugin');
@@ -16,6 +17,7 @@ module.exports = fp((instance, options, next) => {
     instance.register(valuation, options)
     instance.register(dashboard, options)
     instance.register(report_sales_by_item, options)
+    instance.register(purchase, options)
 
     next()
 })
