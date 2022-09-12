947ad92e3eb4f5913242362a65a10cbbe4595313
Umar Akbarov <umarakbarov@Umars-MacBook-Pro.local>
2022-09-12 18:26:58 +0500

    fixed mig

--- services/a.test.queries.js
+++ services/a.test.queries.js
@@ -199,21 +199,21 @@ module.exports = fp((instance, options, next) => {
         for (const org_inv_history of org_inv_histories) {
             let hours_inc = 0
             let minut = 0
-            org_inv_history.histories.sort()
+            org_inv_history.histories.sort((a, b) => a.unique > b.unique ? 1 : -1)
             for (const inv_history of org_inv_history.histories) {
                 const inv_date = new Date(inv_history.date)
-                if (inv_date.getHours() === 0 || inv_date.getHours() === 5) {
-                    inv_date.setHours(9 + hours_inc, minut)
-                    inv_history.date = inv_date.getTime();
-                    hours_inc += parseInt(11 / org_inv_history.histories.length)
-                    minut += parseInt(60 / org_inv_history.histories.length)
-                    changed++
-                    await instance.inventoryHistory.findByIdAndUpdate(
-                        inv_history._id,
-                        { date: inv_history.date },
-                        { lean: true },
-                    )
-                }
+                // if (inv_date.getHours() === 0 || inv_date.getHours() === 5) {
+                inv_date.setHours(9 + hours_inc, minut)
+                inv_history.date = inv_date.getTime();
+                hours_inc += parseInt(11 / org_inv_history.histories.length)
+                minut += parseInt(50 / org_inv_history.histories.length)
+                changed++
+                await instance.inventoryHistory.findByIdAndUpdate(
+                    inv_history._id,
+                    { date: inv_history.date },
+                    { lean: true },
+                )
+                // }
             }
         }
         console.log('end...', changed);
