e21019372a1778dc138ab9022047e56d0d9cca8e
creativeboy1999 <creativeboy1999@github.com>
2022-10-19 18:30:02 +0500

    fixed

--- admin/reports/sales_by_partiation&client.js
+++ admin/reports/sales_by_partiation&client.js
@@ -612,8 +612,8 @@ module.exports = (instance, _, next) => {
     }
     console.log("users_obj", users_obj);
     for (let i = 0; i < result.length; i++) {
-      console.log(users_obj[result[i].user_id]);
-      if ("users_obj[result[i].user_id], result[i].user_id",users_obj[result[i].user_id], result[i].user_id)
+      console.log("users_obj[result[i].user_id], result[i].user_id", users_obj[result[i].user_id]);
+      if (users_obj[result[i].user_id], result[i].user_id)
         result[i].client_name = users_obj[result[i].user_id].first_name + users_obj[result[i].user_id].last_name
       else
         result[i].client_name = ""
