c993054266f699522125fc92c042a6498a2a33ce
creativeboy1999 <creativeboy1999@github.com>
2023-11-02 03:23:40 +0500

    feat added inn /reports/by_receipt/:min/:max/:limit/:page/:type

--- admin/reports/by_receipt.js
+++ admin/reports/by_receipt.js
@@ -235,6 +235,7 @@ module.exports = (instance, _, next) => {
           user_id: 1,
           client_id: 1,
           cashback_phone: 1,
+          cash_back: 1,
           comment: 1,
         }
       },
